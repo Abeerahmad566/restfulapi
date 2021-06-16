62d435b0b2f7998435be1f16a6b769e88c3f724e
Abeer <79338797+Abeer1266@users.noreply.github.com>
2021-06-16 15:46:33 +0500

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,6 +5,25 @@ var bcrypt = require("bcryptjs");
 const _ = require("lodash");
 const jwt = require("jsonwebtoken");
 const config = require("config");
+//get products
+router.get("/allusers",async (req, res) => {
+  let page = Number(req.query.page ? req.query.page : 1);
+  let perPage = Number(req.query.perPage ? req.query.perPage : 10);
+  let skipRecords = perPage * (page - 1);
+  let users = await User.find().skip(skipRecords).limit(perPage);
+  return res.send(users);
+});
+//get single products
+router.get("/allusers/:id", async (req, res) => {
+  try {
+    let user = await User.findById(req.params.id);
+    if (!user)
+      return res.status(400).send("Product With given ID is not present"); //when id is not present id db
+    return res.send(user); //everything is ok
+  } catch (err) {
+    return res.status(400).send("Invalid ID"); // format of id is not correct
+  }
+});
 router.post("/register", async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send("User with given Email already exist");
